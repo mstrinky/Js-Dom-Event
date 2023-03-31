330cec95992b95c7febca4412629f71731ed511c
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-31 10:55:21 +0600

    evenlistener

--- event.js
+++ event.js
@@ -1,3 +1,13 @@
 function makeCyan(){
     document.body.style.backgroundColor='cyan';
 }
+
+const makeGreen = document.getElementById('make-green');
+        makeGreen.addEventListener('click',function makeGreen(){
+            document.body.style.backgroundColor = 'green';
+})
+
+const makeGoldenRod = document.getElementById('make-goldenrod').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
+
